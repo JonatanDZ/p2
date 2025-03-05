38eea3a43d63226b789c057540f55b7f977fff37
JonatanDZ <jmuhle24@student.aau.dk>
2025-03-05 13:00:21 +0100

    Test 1 af searchbar

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,12 @@ import styles from "../styles/Footer.module.css";
 
 export default function Footer() {
     return (
-      <footer className={styles.footer}>
-        <p>© {new Date().getFullYear()} My Next.js App. All rights reserved.</p>
-      </footer>
+        <footer className={styles.footer}>
+            <div className={styles.footerInfo}>
+                <p>© {new Date().getFullYear()} My Next.js App. All rights reserved.</p>
+
+            </div>
+        </footer>
     );
-  }
+}
   
