1088a67e90092511d52c58e316aa7d0bc7a832c0
JonatanDZ <jmuhle24@student.aau.dk>
2025-03-10 10:38:15 +0100

    landing page update

--- src/app/landing/page.js
+++ src/app/landing/page.js
@@ -1,7 +1,20 @@
+import style from "../../styles/LandingPageBox.module.css";
+
 export default function Landing() {
     return (
       <main>
-        <h1>Test</h1>
+        <h1 className={style.header}>Vælg din lokale by du vil handle i.</h1>
+        <div className={style.landingPage}>
+          <div className={style.landingPageBox}>
+            <p>Aalborg</p>
+          </div>
+          <div className={style.landingPageBox}>
+            <p>København</p>
+          </div>
+          <div className={style.landingPageBox}>
+            <p>Aarhus</p>
+          </div>
+          </div>
       </main>
     );
   }
