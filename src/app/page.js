6e4d8828d46aaa9b35751bcbda84ac971a95d83e
JonatanDZ <jmuhle24@student.aau.dk>
2025-03-10 14:08:20 +0100

    Start på main

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,24 @@
 import '../styles/global.css';
+import style from '../styles/MainPage.module.css';
 
 export default function Home() {
   return (
     <main>
+      <div className={style.main}>
+        <div className={style.slideshow}>
+          <img className={style.slideshowImage} src="/image_aalborg.png" alt="slideshow1" />
+          <img className={style.slideshowImage} src="/image_aalborg.png" alt="slideshow2" />
+          <img className={style.slideshowImage} src="/image_aalborg.png" alt="slideshow3" />
+          <img className={style.slideshowImage} src="/image_aalborg.png" alt="slideshow4" />
+          <img className={style.slideshowImage} src="/image_aalborg.png" alt="slideshow5" />
+        </div>
+        <div className={style.recommendations}> 
+        </div>
+        <div className={style.newArrivals}>
+
+        </div>
+      </div>
+
     </main>
   );
 }
