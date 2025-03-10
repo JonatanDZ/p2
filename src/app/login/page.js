381620a8e8f04422d16f16b6b194ae82df1c738c
Nikolaidk3 <hp82fk@student.aau.dk>
2025-03-10 12:25:04 +0100

    login page
    tilføjet login mappe med en login page
    dertil tilføjet en mappe "authentication"

---
+++ src/app/login/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import LoginComponent from './login';
+import './page.css';
+
+export default function Login() {
+  return (
+    <div className="main-container">
+       <LoginComponent />
+    </div>
+  );
+}
