da1bfc759e0d877e6baf86f6f812e656ede765d0
JonatanDZ <jmuhle24@student.aau.dk>
2025-03-10 13:13:58 +0100

    img added

--- src/app/landing/page.js
+++ src/app/landing/page.js
@@ -1,18 +1,24 @@
 import style from "../../styles/LandingPageBox.module.css";
+import Link from "next/link";
 
 export default function Landing() {
     return (
       <main>
-        <h1 className={style.header}>Vælg din lokale by du vil handle i.</h1>
+        <h1 className={style.header}>Make local greate again</h1>
         <div className={style.landingPage}>
+          <Link href="/">
           <div className={style.landingPageBox}>
-            <p>Aalborg</p>
+              <p>Aalborg</p>
+              <img src="/image_aalborg.png" alt="Aalborg" />
           </div>
+          </Link>
           <div className={style.landingPageBox}>
             <p>København</p>
+            <img src="/kbh.jpg" alt="København" />
           </div>
           <div className={style.landingPageBox}>
             <p>Aarhus</p>
+            <img src="/aarhus.jpg" alt="Aarhus" />
           </div>
           </div>
       </main>
