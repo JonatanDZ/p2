96dcf884f654bc59af85e052db846b39ccbb59c7
Nikolaidk3 <hp82fk@student.aau.dk>
2025-03-18 14:20:58 +0100

    validering af "confirm password" samt "signup" kanp ikke kan trykkes på medmindre alt er udfyldt korrekt

--- src/app/login/signup/signup.js
+++ src/app/login/signup/signup.js
@@ -1,12 +1,46 @@
-import react from "react";
-import styles from "../../../styles/Login.module.css";
-import { save_user } from "../../actions";
+"use client"
 
-const signupComponent = () => {
+import { useState } from "react";
+import styles from "../../../styles/Login.module.css";
+
+const SignupComponent = () => {
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [isPasswordValid, setIsPasswordValid] = useState(true);
+
+  const handlePasswordChange = (e) => {
+    if(e.target.value !== confirmPassword) {
+      setIsPasswordValid(false);
+    } else {
+      setIsPasswordValid(true);
+    }
+  };
+
+  const handleConfirmPasswordChange = (e) => {
+    setConfirmPassword(e.target.value);
+
+    if (password !== e.target.value) {
+      setIsPasswordValid(false);
+    } else {
+      setIsPasswordValid(true);
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (password != confirmPassword){
+      setIsPasswordValid(false);
+      console.log("passwords do not match");
+      return;
+    }
+    console.log("success");
+  }
+
   return (
     <div className={styles.container}>
-      <form onSubmit={save_user}>
+      <form onSubmit={handleSubmit}>
         <h1 className={styles.loginname}>Registrer dig</h1>
+        
         <div className={styles.inputBox}>
           <input
             type="email"
@@ -15,12 +49,15 @@ const signupComponent = () => {
             required
           ></input>
         </div>
+
         <div className={styles.inputBox}>
           <input
             type="password"
             placeholder="Password"
             name="password"
             required
+            value={password}
+            onChange={handlePasswordChange}
           ></input>
         </div>
 
@@ -28,15 +65,18 @@ const signupComponent = () => {
           <input
             type="password"
             placeholder="Confirm Password"
+            name="confirmPassword"
             required
+            value={confirmPassword}
+            onChange={handleConfirmPasswordChange}
           ></input>
         </div>
 
         <button type="subimt" className={styles.button}>Signup</button>
 
-        <div className={styles.registerLink}>
+        <div className={styles.registerLink} disabled={password != confirmPassword}>
           <p>
-            Allerede bruger? <a href="#">Login her</a>
+            Allerede bruger? <a href="">Login her</a>
           </p>
         </div>
       </form>
@@ -44,4 +84,4 @@ const signupComponent = () => {
   );
 };
 
-export default signupComponent;
+export default SignupComponent;
